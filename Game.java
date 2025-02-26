4a4f70e888ff55c22241ee59ea9a4296ccca9c88
Thaïs Castillo <thais.castillo2005@gmail.com>
2025-02-26 23:06:57 +0100

    Update Game.java

--- Game.java
+++ Game.java
@@ -3,8 +3,16 @@ public class Game {
     private Engine aEngine;
 
     public Game(){
-        this.aEngine = new Engine(new Player("A",1), new Player("B",2));
-        this.aUI = new UserInterface(aEngine);
+        this.aEngine = new Engine();
+        this.aUI = new UserInterface(this.aEngine);
         this.aEngine.setUI(this.aUI);
+        String pseudo1 = aUI.getPseudo();
+        String pseudo2 = aUI.getPseudo1();
+        Player p1 = new Player(pseudo1, 1);
+        Player p2 = new Player(pseudo2, 2);
+        this.aEngine.setP1(p1);
+        this.aEngine.setP2(p2);
+        System.out.println("Pseudo récupéré pour aP1 : " + pseudo1);
+        System.out.println("Pseudo récupéré pour aP2 : " + pseudo2);
     }
 }
