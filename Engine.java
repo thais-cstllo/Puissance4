33a37abc9e35773016d4991f70e9df3b47c0c727
Thaïs Castillo <thais.castillo2005@gmail.com>
2025-02-26 23:06:15 +0100

    Update Engine.java

--- Engine.java
+++ Engine.java
@@ -6,9 +6,7 @@ public class Engine {
     private UserInterface aUI;
 
 
-    public Engine(Player aP1, Player aP2) {
-        this.aP1 = aP1;
-        this.aP2 = aP2;
+    public Engine() {
         this.aGrid = new Grid();
         this.aMainPlayer = this.aP1;
     }
@@ -17,15 +15,28 @@ public class Engine {
         this.aUI = pUI;
     }
 
+    public void setP2(Player aP2) {
+        this.aP2 = aP2;
+    }
+
+    public void setP1(Player p1) {
+        this.aP1 = p1;
+    }
+    public void setaMainPlayer() {
+        this.aMainPlayer = this.aP1;
+    }
+
     public Grid getGrid() {return this.aGrid;}
 
     public void move(final int column) {
-        int vId = this.aMainPlayer.getId();
-
+        setaMainPlayer();
+        System.out.println("aP1 : " + this.aP1.getName());
+        System.out.println("aP2 : " + this.aP2.getName());
+        String vNom = this.aMainPlayer.getName();
         this.aGrid.changeCellColor(column,this.aMainPlayer.getId()); // Insère le pion dans la colonne choisie
         if(this.checkVictory(this.aMainPlayer)) {
-            System.out.printf("le joueur " + vId + " a gagné ! ");  // Vérifie si le joueur a gagné
-            this.aUI.afficheVictoire(vId);
+            System.out.printf("le joueur " + vNom+ " a gagné ! ");  // Vérifie si le joueur a gagné
+            this.aUI.afficheVictoire(vNom);
             this.endGame();
             return;
         }
@@ -39,12 +50,12 @@ public class Engine {
     /**
      * Methode à utiliser après le jeu du joueur pour faire changer le joueur principal
      */
-    public void alternate() {
-        if (this.aMainPlayer.equals(this.aP1))
-            this.setMainPlayer(this.aP2);
-        else
-            this.setMainPlayer(this.aP1);
-    }
+        public void alternate() {
+            if (this.aMainPlayer.equals(this.aP1))
+                this.setMainPlayer(this.aP2);
+            else
+                this.setMainPlayer(this.aP1);
+        }
 /*
     public Player alternate2() {
         if (this.aMainPlayer == this.aP1.getId()){
