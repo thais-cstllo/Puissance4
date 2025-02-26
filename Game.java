6b47f07224b8c42ce449163c76cf079f8e0a3ebc
Thaïs Castillo <thais.castillo2005@gmail.com>
2025-02-26 19:32:10 +0100

    Update Game.java
    Problème à résoudre : Je ne peux pas prendre les nom des utilisateurs si je n'instancie pas UI au début et je ne peux pas créer Ui sans de Engine, donc j'ai créé setUI, mais les nom des joueurs sont null dans le jeu car lorsque j'instancie UI, qui dans le constructeur n'a pas createUI (tu verras qu'il est impossible pour moi de le faire), les noms ne sont donc pas prit en compte.

--- Game.java
+++ Game.java
@@ -3,8 +3,16 @@ public class Game {
     private Engine aEngine;
 
     public Game(){
-        this.aEngine = new Engine(new Player("A",1), new Player("B",2));
-        this.aUI = new UserInterface(aEngine);
+        this.aUI = new UserInterface();
+
+        String pseudo1 = aUI.getPseudo();
+        String pseudo2 = aUI.getPseudo1();
+        //Le problème est que lorsque que l'on créé l'objet UserInterface, createUI n'est pas créé donc on ne peut pas prendre le nom des utilsateurs
+        this.aEngine = new Engine(new Player(pseudo1, 1), new Player(pseudo2, 2));
+        System.out.println("Pseudo récupéré pour aP1 : " + pseudo1);
+        System.out.println("Pseudo récupéré pour aP2 : " + pseudo2);
+        this.aUI.setEngine(this.aEngine);
         this.aEngine.setUI(this.aUI);
+
     }
 }
